5426d5d551462a3ce6eb902a4c7f1752a08acc4c
Amos Wenger <amos@bearcove.eu>
2025-12-14 16:03:41 +0100

    Expand KDL highlight captures

--- langs/group-maple/kdl/def/queries/highlights.scm
+++ langs/group-maple/kdl/def/queries/highlights.scm
@@ -1,50 +1,65 @@
-; Node names
+; --------------------------------------------------------------------------------
+; Node + structure
+; --------------------------------------------------------------------------------
 
-(node_no_terminator name: (string) @type)
+(node_no_terminator
+  (slashdash)? @comment
+  name: (string) @type.definition)
 
+(node_children) @scope
+
+; Allow top-level identifiers (nodes without string quoting) to appear as types
+(identifier_string) @variable
+
+; --------------------------------------------------------------------------------
+; Properties / arguments
+; --------------------------------------------------------------------------------
+
+(prop key: (string) @property)
+(node_prop_or_arg (value (type) @type))
+
+; --------------------------------------------------------------------------------
 ; Type annotations
+; --------------------------------------------------------------------------------
 
 (type) @type
 (annotation_type) @type.builtin
 
-; Properties
-
-(prop key: (string) @property)
-
-; Identifiers
-
-(identifier_string) @variable
-
+; --------------------------------------------------------------------------------
 ; Literals
+; --------------------------------------------------------------------------------
 
 (quoted_string) @string
 (raw_string) @string
-
 (escape) @string.escape
 (ws_escape) @string.escape
 
-(number) @number
+[(boolean) (keyword)] @constant.builtin
 (keyword_number) @number.special
+(number) @number
 
-(boolean) @boolean
+; Hash keywords (#true/#false/#null/#inf/etc)
+(keyword) @keyword
 
-(keyword) @constant.builtin
-
-; Operators / punctuation
+; --------------------------------------------------------------------------------
+; Punctuation / operators
+; --------------------------------------------------------------------------------
 
 "=" @operator
-
 ["{" "}"] @punctuation.bracket
 ["(" ")"] @punctuation.bracket
-
 ";" @punctuation.delimiter
 
-; Comments
+; --------------------------------------------------------------------------------
+; Comments & directives
+; --------------------------------------------------------------------------------
 
 [
   (single_line_comment)
   (multi_line_comment)
 ] @comment @spell
 
-; Slashdash directives disable nodes and are treated like comments in KDL
 (slashdash) @comment
+
+; Version marker (/- kdl-version ...)
+(version_marker) @keyword.directive
